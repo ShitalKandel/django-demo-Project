ab36595861ffa7b23513d6b91e14b72e8274e513
Shital Kandel <kandelshittal@gmail.com>
2024-01-26 13:18:08 +0545

    Django Learning

--- django_Project/app/views.py
+++ django_Project/app/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hi , There")
-
-# Create your views here.
+    return HttpResponse("Hello, world. You're at the polls index.")
